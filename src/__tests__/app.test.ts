442a5265ddad3986046dadac75e650b3b6811840
Rick Kock <rit.kock@gmail.com>
2022-12-01 22:22:46 +0100

    Test: get single todo item by id

--- src/__tests__/app.test.ts
+++ src/__tests__/app.test.ts
@@ -4,17 +4,23 @@ import app from "../app";
 
 describe("Todos API", () => {
   const todosStub = [
-    { todo: "Todo list item 1" },
-    { todo: "Todo list item 2" },
+    { id: "1", todo: "Todo list item 1" },
+    { id: "2", todo: "Todo list item 2" },
   ];
 
-  it("GET /todos --> array todos", async () => {
+  it("GET /todos --> Array of todos", async () => {
     const response = await request(app).get(`/todos`);
     expect(response.status).toBe(200);
     expect(response.body).toEqual(expect.arrayContaining(todosStub));
   });
 
   // it("GET /todos/id --> specific todo by ID", () => {});
+  it("GET /todos/:id --> Single todo item", async () => {
+    const todoId = 2;
+    const response = await request(app).get(`/todos/${todoId}`);
+    expect(response.status).toBe(200);
+    expect(response.body).toEqual(todosStub[todoId - 1]);
+  });
 
   // it("POST /todos --> created todo", () => {});
 
--- src/app.ts
+++ src/app.ts
@@ -1,11 +1,20 @@
 import express, { Application } from "express";
 
+const todos = [
+  { id: "1", todo: "Todo list item 1" },
+  { id: "2", todo: "Todo list item 2" },
+];
+
 const app: Application = express();
 
 app.use(express.json());
 
 app.get("/todos", (req, res) => {
-  res.send([{ todo: "Todo list item 1" }, { todo: "Todo list item 2" }]);
+  res.send(todos);
+});
+
+app.get("/todos/:id", (req, res) => {
+  res.status(200).send(todos.find((todos) => todos.id === req.params.id));
 });
 
 export default app;
